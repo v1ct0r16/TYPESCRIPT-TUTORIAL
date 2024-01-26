5b5cd376b3a91de29abaf0a333b8f24f1f9e5461
v1ct0r16 <pvictor1844@gmail.com>
2024-01-26 16:50:31 +0100

    day 2 commit

---
+++ variable/variable.js
@@ -0,0 +1,15 @@
+var victor = "victor";
+var promise = false;
+var lookup = [victor, promise, 23, 30, "red", "green", "yellow"];
+console.log(lookup[0]);
+console.log(lookup[1]);
+var legend = victor;
+var addNumber = function () {
+    return;
+};
+function addsomehting() {
+}
+var addName = function () {
+    return "welcome to arrow functions";
+    addName();
+};
---
+++ variable/variable.ts
@@ -0,0 +1,22 @@
+const victor:string = "victor";
+
+const promise:boolean = false;
+
+const lookup= [victor, promise, 23, 30, "red", "green", "yellow"]
+
+console.log(lookup[0]);
+console.log(lookup[1]);
+
+const legend:any = victor;
+
+const addNumber = () =>{
+return
+}
+
+function addsomehting () {
+
+}
+
+const addName = () => {return "welcome to arrow functions"
+ addName ();}
+ 
